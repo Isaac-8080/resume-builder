6c4660de3d8f9caa9e43d82369310387240252a2
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-22 22:21:25 +0000

    update made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-
+import './App.css'
+import Home from "./Pages/Home"
+import TemplatesPage from "./Pages/TemplatesPage"
+import TemplateOne from './Pages/TemplateOne'
+import TemplateTwo from './Pages/TemplateDetail'
+import PageNotFound from "./Pages/PageNotFound"
+import RootLayout from "./Layouts/RootLayout"
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from "react-router-dom"
 
 function App() {
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="templates" element={<TemplatesPage /> } />
+        <Route path="templates/templateOne" element={<TemplateOne />} />
+        <Route path="templates/templateTwo" element={<TemplateTwo />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    )
+  )
+
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
+  
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBg: '#1F2937',
+        whiteText: '#F2F2F2',
+      }
+    },
   },
   plugins: [
     require('daisyui'),
