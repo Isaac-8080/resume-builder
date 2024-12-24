fe02412fae38e06813077bef55f4a7e0a9ee20a8
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-24 22:34:14 +0000

    Home added

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+
+const Home = () => {
+  return (
+    <>
+      <div className=" w-full flex flex-col items-center mt-20">
+        <p className="text-5xl font-bold">What Will You Design Today?</p>
+        {/* <p className="text-2xl">Under Construction</p> */}
+      </div>
+
+    </>
+  )
+}
+
+export default Home
