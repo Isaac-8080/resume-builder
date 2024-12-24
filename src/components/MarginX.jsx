d463dd92383cfd10096f22ff0d645872f5b0b8ec
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-24 22:47:56 +0000

    component added

---
+++ src/components/MarginX.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MarginX = (props) => {
+  return (
+    <>
+      <div className={`${props.classItem} px-5 lg:mx-24`}>
+        {props.children}
+      </div>
+    </>
+  )
+}
+
+export default MarginX
