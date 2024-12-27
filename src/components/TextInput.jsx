0e35ce675b2e947a832b0fe81e831361e603f2eb
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-12-27 04:30:39 +0000

    component added

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,28 @@
+
+const TextInput = (props) => {
+  return (
+    <>
+      <label>
+        {props.labelText}
+        {
+          props.value.length === 0 ? <input
+            type="text"
+            className={"border p-2.5 rounded mt-1 outline-0 focus-within:border-red-500 w-full block"}
+            value={props.value}
+            onChange={props.onChange}
+            placeholder={props.placeholder}
+          /> : <input
+            type="text"
+            className={"border p-2.5 rounded mt-1 outline-0 focus-within:border-green-500 w-full block"}
+            value={props.value}
+            onChange={props.onChange}
+            placeholder={props.placeholder}
+          />
+        }
+       
+      </label>
+    </>
+  )
+}
+
+export default TextInput
